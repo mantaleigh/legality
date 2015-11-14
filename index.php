9b563eeb414d3f16c7ece150137c1b55c5eb91e9
Samantha Voigt <samantha@Samanthas-MacBook-Pro.local>
2015-11-13 23:24:48 -0500

    finished setup

--- index.php
+++ index.php
@@ -20,28 +20,45 @@ $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die("Failed to c
 ?>
 
 
-<html>
+<html lang="en">
 
 <head>
-<script type = "text/javascript">
- //Here we take the PHP data through ajax
-</script>
-<link rel="stylesheet" href="css/styling.css" type="text/css">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Legality</title>
+    
+    <!-- Boostrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+    <script type = "text/javascript">
+     //Here we take the PHP data through ajax
+    </script>
+    
+    <!-- Custom Styling -->
+    <link rel="stylesheet" href="css/styling.css" type="text/css">
+    
 </head>
-
-
-
-
-<body>
-
-<!-- our page --> 
-<h1> testing </h1>
     
-    
-<script type="text/javascript" src="js/jquery-1.11.3.js"></script>
-<script type="text/javascript" src="js/test.js"></script>
-</body>
+    <body>
+        <h1> testing </h1>
 
+        
+        
+        
+        
 
-
+        <!-- jQuery --> 
+        <script type="text/javascript" src="js/jquery-1.11.3.js"></script>
+        <!-- Boostrap --> 
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- Custom -->
+        <script type="text/javascript" src="js/test.js"></script>
+    </body>
 </html>
